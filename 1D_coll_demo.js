03c775e3d36438f182fcebf7efe47f381b5d39bf
William Newell <newellwm@gmail.com>
2016-07-21 21:35:27 -0400

    Remove collision concept, Add ship object

---
+++ 1D_coll_demo.js
@@ -0,0 +1,58 @@
+//DEMO CODE COLLISION
+  var $gameWindow = $('#game-window');
+  var $spaceship = $('<div class="spaceship">');
+  $gameWindow.append($spaceship);
+  var windowLoc = $gameWindow.offset();
+
+  // for (var i=0;i<2;i++){
+    var $alienship = $('<div class="alienship">');
+    $gameWindow.append($alienship);
+    $alienship.css("top", Math.random() * $gameWindow.innerHeight()+windowLoc.top);
+    // $alienship.eq(i).css("left", Math.random() * $gameWindow.innerWidth()+windowLoc.left);
+  // }
+  var $alienships = $('.alienship');
+
+  // setInterval(function() {
+  //   for(var i=0;i<$alienships.length;i++){
+  //     $alienships.eq(i).css("top", Math.random() * $gameWindow.innerHeight()+windowLoc.top);
+  //     $alienships.eq(i).css("left", Math.random() * $gameWindow.innerWidth()+windowLoc.left);
+  //   }
+  // }, 2000)
+
+
+  setInterval(function() {
+    var t = Math.random() * $gameWindow.innerHeight()+windowLoc.top;
+    var l = Math.random() * $gameWindow.innerWidth()+windowLoc.left;
+
+    $alienships.animate({
+      top: t,
+      left: l
+    }, 2000)
+  }, 2001)
+
+  setInterval(function(){
+    console.log($alienship.offset())
+  },1000);
+  // setInterval(function(){
+  //   //location should be object property
+  //   var shipLoc = $spaceship.offset()
+  //   var alienLoc = [];
+  //   for(var i=0;i<$alienship.length;i++){
+  //     alienLoc[i] = $alienship.eq(i).offset();
+  //   }
+  //   for(var i=0;i<$alienship.length;i++){
+  //     console.log(alienLoc[i])
+  //   }
+  // },16)
+  $gameWindow.on('mousemove',function(event){
+    console.log(event.pageY)
+    $spaceship.css({'top':event.pageY,'left':event.pageX});
+    var alienLoc = $alienship.offset().left;
+    console.log(alienLoc.top)
+    dist = Math.abs(alienLoc.top-event.pageY);
+    console.log(dist)
+    if (dist < 90)
+      $spaceship.addClass('collide')
+    else
+      $spaceship.removeClass('collide')
+  })
--- js/object.js
+++ js/object.js
@@ -1,4 +1,22 @@
-// var Ship = function(){
-
-// }
-// Ship.prototype.fire = function{}
+"use strict";
+var UserShip = function(){
+  this.health = 3;
+  this.airbrake = 3;
+}
+UserShip.prototype.reset = function(){
+  this.health--;
+}
+UserShip.prototype.create = function(){
+  var $spaceship = $('<div class="spaceship">');
+  //$spaceship.css({top:$gameWindow.height()/2,
+  //                left:$gameWindow.width()/2});
+  for (var i=0;i<this.health;i++){
+    console.log('gaytest')
+      var $health = $('<li class="health">');
+      var $airbrake = $('<li class="airbrake">');
+      $('#ship-health').append($health);
+      $('#ship-powerups').append($airbrake);
+  }
+  $('#game-window').append($spaceship);
+  return('SUPER GAY')
+}
