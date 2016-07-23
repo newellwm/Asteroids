0bf1c316debeb49dc73b441693b36a0c42a44bbf
William Newell <newellwm@gmail.com>
2016-07-23 14:45:10 -0400

    Add position updating and acceleration to spaceship

--- js/object.js
+++ js/object.js
@@ -1,22 +1,59 @@
 "use strict";
+
+var $gameWindow = $('#game-window');
 var UserShip = function(){
   this.health = 3;
   this.airbrake = 3;
+  this.invincible = true;
+  this.accelKeyPress = false;
+  this.velocity = [0,0];
+  this.position = [];
+  this.angle=0;
+  this.time = Date.now();
+  this.turn = 'stop';
 }
 UserShip.prototype.reset = function(){
   this.health--;
+  this.invincible = true;
+  $spaceship.css({top: $gameWindow.offset().top+$gameWindow.innerHeight()/2-48,
+                 left: $gameWindow.offset().left+$gameWindow.innerWidth()/2-48});
 }
 UserShip.prototype.create = function(){
-  var $spaceship = $('<div class="spaceship">');
-  //$spaceship.css({top:$gameWindow.height()/2,
-  //                left:$gameWindow.width()/2});
+  var $spaceship = $('<div class="spaceship id=user-ship">');
+  //THE '48' VALUES ARE BASED FROM THE .CSS HEIGHT/WIDTH OF $spaceship
+  $spaceship.css({top: $gameWindow.offset().top+$gameWindow.innerHeight()/2-48,
+                 left: $gameWindow.offset().left+$gameWindow.innerWidth()/2-48});
+  this.position[0] = Number($spaceship.css('left').split('px').shift());
+  this.position[1] = Number($spaceship.css('top').split('px').shift());
   for (var i=0;i<this.health;i++){
-    console.log('gaytest')
       var $health = $('<li class="health">');
       var $airbrake = $('<li class="airbrake">');
       $('#ship-health').append($health);
       $('#ship-powerups').append($airbrake);
   }
-  $('#game-window').append($spaceship);
-  return('SUPER GAY')
+  $gameWindow.append($spaceship);
+  return($spaceship);
+}
+UserShip.prototype.setPosition = function(x,y,ship,dt){
+$(ship).animate({top: y,
+            left: x},dt);
+}
+UserShip.prototype.setRotation = function(){
+  switch (this.turn){
+    case 'right':
+      this.
+      break;
+    case 'left':
+      break;
+  }
+}
+var Meteor = function(){
+  this.size = 3;
+  this.health = 2;
+}
+Meteor.prototype.split = function(){
+}
+
+var EnemyShip = function(){
+  this.health = 3;
 }
--- js/script.js
+++ js/script.js
@@ -1,14 +1,127 @@
 "use strict";
-$(function(){
-  var
-    $gameWindow = ('#game-window'),
-    myShip = new UserShip(),
-    myShipDiv = myShip.create();
-    console.log(myShipDiv);
-})
+
 //code sites referenced
 
 //mimic'd organization:
 //http://blog.sklambert.com/html5-canvas-game-the-player-ship/
 
-//
+var eventListeners = function(){
+
+  // Up
+  $(window).keydown(function(event){
+    if(event.which==87){
+      myShip.accelKeyPress = true;
+    }
+  })
+  $(window).keyup(function(event){
+    if(event.which==87){
+      myShip.accelKeyPress = false;
+    }
+  })
+  //  Right
+  $(window).keydown(function(event){
+    if(event.which==68){
+      myShip.turn = 'right';
+    }
+  })
+  $(window).keyup(function(event){
+    if(event.which==68){
+      myShip.turn = 'stop';
+    }
+  })
+  // Left
+  $(window).keydown(function(event){
+    if(event.which==65){
+      myShip.turn = 'left';
+    }
+  })
+  $(window).keyup(function(event){
+    if(event.which==65){
+      myShip.turn = 'stop';
+    }
+  })
+}
+
+// CHECK ALL METEORS FOR COLLISION
+// for (var i=mtr.length;i>0;i--){
+//   for (var j=0;j<i;j++){
+//     collCheck(i,j)
+//   }
+// }
+
+// CHECK FOR SHIP INVINCIBILITY
+// if (myShip.invincible === true && invCounter < 3000){
+//   invCounter += 20;
+// }else if (myShip.invincible === true){
+//   myShip.invincible = false
+// }else{
+//   //collision logic
+// }
+  var
+    myShip = new UserShip(),
+    myShipDiv = myShip.create();
+
+var shipPosition = function(){
+//ACCELERATION on keypress pass boolean and t0 to ship object, on key release store
+  myShip.setRotation();
+  var
+    v0bar = myShip.velocity,
+    d0bar = myShip.position,
+    theta = (myShip.angle+90)*Math.PI/180,
+    d1bar = [],
+    v1bar = [],
+    t1 = Date.now(),
+    dt = t1-myShip.time,
+    acc = 1/4500,
+    maxV = 1/90,
+    v0 =0,
+    v1 = 0;
+    if (v0bar[0]===0&&v0bar[1]===0){
+      v0 = 0;
+    }else{
+      v0=Math.sqrt(Math.pow(v0bar[0],2)+Math.pow(v0bar[1],2));
+    }
+                                                       //console.log(v0)
+  // if (theta>180){
+  //   theta-=180;
+  // }
+
+  //ACCELERATION CONTROL && VELOCITY CALCS
+  if (myShip.accelKeyPress === true){
+    if(v0>=maxV){
+      v1 = maxV;
+    }else{
+      v1 = v0+dt*acc;
+    }
+  }else if(v0 >= 0){
+    if(v0>=maxV){
+      v1 = maxV;
+    }else{
+      v1 = v0-dt*acc;
+    }
+  }
+                                                     // console.log(dt);
+                                                  // console.log(theta);
+  v1bar[0] = v1*Math.cos(theta);
+  v1bar[1] = v1*Math.sin(theta);
+  // if(v0bar[0]>0){
+  //   v0bar[0] = 0;
+  // }
+  // if(v0bar[1]>0){
+  //   v0bar[1] = 0;
+  // }
+  //UPDATE LOCATION
+                                              // console.log(d0bar+'d0')
+  d1bar[0] = d0bar[0]+dt*v1bar[0];
+  d1bar[1] = d0bar[1]+dt*v1bar[1];
+  myShip.setPosition(d0bar[0],d0bar[1],myShipDiv,dt);
+  myShip.velocity = v1bar;
+  myShip.position = d1bar;
+  myShip.time = t1;
+}
+
+$(function(){
+    eventListeners();
+    setInterval(shipPosition,50);
+})
+
