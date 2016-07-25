837654d7dc4760489710da0b2cb9a4f92a7c2854
William Newell <newellwm@gmail.com>
2016-07-25 10:18:17 -0400

    Reduce default asteroid count to 6

--- js/function.js
+++ js/function.js
@@ -6,7 +6,7 @@ var
   myShip = [];
   $myShip = [];
 var spawnAsteroids = function(input){
-  for(var i=0;i<10;i++){
+  for(var i=0;i<6;i++){
     asteroids[i] = new Asteroid();
     asteroids[i].createRand();
   }
